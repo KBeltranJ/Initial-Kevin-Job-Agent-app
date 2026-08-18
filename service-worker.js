cba0fa23dd6c439990284ab81014ef8fe680e075
Kevin Beltran <44617053+KBeltranJ@users.noreply.github.com>
2026-08-18 00:13:13 -0400

    Cache applied jobs search update

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="kevin-job-agent-v6";
-const ASSETS=["./","./index.html","./styles.css","./custom-jobs.css","./app.js","./sync.js","./custom-jobs.js","./manifest.webmanifest","./jobs.json","./icons/icon-192.png","./icons/icon-512.png","./icons/maskable-512.png"];
+const CACHE="kevin-job-agent-v7";
+const ASSETS=["./","./index.html","./styles.css","./custom-jobs.css","./applied-search.css","./app.js","./sync.js","./custom-jobs.js","./applied-search.js","./manifest.webmanifest","./jobs.json","./icons/icon-192.png","./icons/icon-512.png","./icons/maskable-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{
